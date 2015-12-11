49b49415f44ac38ff533410dd71c5b317ae4f48e
dengqiming <dengqiming@xiaomadada.com>
2015-12-12 06:48:24 +0800

    wiredep and karma-phastomjs-launcher

--- templates/common/root/_gulpfile.js
+++ templates/common/root/_gulpfile.js
@@ -168,7 +168,7 @@ gulp.task('clean:dist', function (cb) {
   rimraf('./dist', cb);
 });
 
-gulp.task('client:build', ['html', 'styles'], function () {
+gulp.task('client:build', ['bower', 'html', 'styles'], function () {
   var jsFilter = $.filter('**/*.js');
   var cssFilter = $.filter('**/*.css');
   var csjsFilter = $.filter(['**/*.js','**/*.css']);
